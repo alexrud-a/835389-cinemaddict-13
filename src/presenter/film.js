e6ee4118e890a0d625bcc7deb803cf197d333c6e
AleXandra Rudivickaya <rag@one-touch.ru>
2020-12-09 01:10:10 +0300

    добавление класса презентера

---
+++ src/presenter/film.js
@@ -0,0 +1,12 @@
+export default class Film {
+  constructor() {
+  }
+
+  init() {
+
+  }
+
+  destroy() {
+
+  }
+}
---
+++ src/presenter/films.js
@@ -0,0 +1,54 @@
+import SiteMenu from "./view/menu";
+import SortPanel from "./view/sort-panel";
+import FilmList from "./view/films-list";
+import FooterStatistics from "./view/count-films";
+import CardFilm from "./view/film-card";
+import Loadmore from "./view/loadmore";
+import Popup from "./view/popup";
+import EmptyFilms from "./view/empty-films";
+import {render, RenderPosition, compareValues} from "./utils.js";
+import Comments from "./view/comments";
+
+const FILM_PER_PAGE = 5;
+
+export default class FilmsList {
+  constructor() {
+
+  }
+
+  init() {
+
+  }
+
+  _renderFilms() {
+
+  }
+
+  _renderMenu() {
+
+  }
+
+  _renderSort() {
+
+  }
+
+  _renderCard() {
+
+  }
+
+  _renderLoadMore() {
+
+  }
+
+  _renderFooter() {
+
+  }
+
+  _renderPopup() {
+
+  }
+
+  _renderEmpty() {
+
+  }
+}
